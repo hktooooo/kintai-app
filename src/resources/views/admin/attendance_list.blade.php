4ec0ab11cc1105278c2c39f0438d433c73534486
kato hideaki <mamamacha88@gmail.com>
2025-11-22 16:52:47 +0900

    スタイル編集

--- src/resources/views/admin/attendance_list.blade.php
+++ src/resources/views/admin/attendance_list.blade.php
@@ -5,50 +5,49 @@
 @endsection
 
 @section('content')
-<div class="attendance__list__content__wrapper">
-    @php
-        $formatted = $current->format('Y年m月d日');
-    @endphp
-    <div class="attendance__list__content">
-        <h1 class="attendance__list__header">{{ $formatted }}の勤怠</h1>
+@php
+    $formatted = $current->format('Y年m月d日');
+@endphp
+<div class="attendance__list__content">
+    <h1 class="attendance__list__header">{{ $formatted }}の勤怠</h1>
 
-        <div class="attendance__list__month-select-box">
-            <a class="attendance__list__prev-next-month" href="{{ url('/admin/attendance/list?day=' . $prevDay) }}"><img src="{{ asset('images/image_prev.png') }}" alt="arrow_prev"><span>前日</span></a>
-            <div class="attendance__list__this-month-calendar">
-                <img src="{{ asset('images/image_calendar.png') }}" alt="calendar">
-                <p class="attendance__list__this-month">{{ $current->format('Y/n') }}</p>
-            </div>
-            <a class="attendance__list__prev-next-month" href="{{ url('/admin/attendance/list?day=' . $nextDay) }}"><span>翌日</span><img src="{{ asset('images/image_next.png') }}" alt="arrow_next"></a>
+    <div class="attendance__list__day-select-box">
+        <a class="attendance__list__prev-next-day" href="{{ url('/admin/attendance/list?day=' . $prevDay) }}"><img src="{{ asset('images/image_prev.png') }}" alt="arrow_prev"><span>前日</span></a>
+        <div class="attendance__list__this-day-calendar">
+            <img src="{{ asset('images/image_calendar.png') }}" alt="calendar">
+            <p class="attendance__list__this-day">{{ $current->format('Y/n/d') }}</p>
         </div>
-
-        <table class="attendance__list__table">
-            <thead>
-                <tr class="attendance__list__row">
-                    <th class="attendance__list__label">名前</th>
-                    <th class="attendance__list__label">出勤</th>
-                    <th class="attendance__list__label">退勤</th>
-                    <th class="attendance__list__label">休憩</th>
-                    <th class="attendance__list__label">合計</th>
-                    <th class="attendance__list__label">詳細</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($attendances as $attendance)
-                    <tr class="attendance__list__row">
-                        <td class="attendance__list__data">{{ $attendance->user->name ?? '不明' }}</td>
-                        <td class="attendance__list__data">{{ \Carbon\Carbon::parse($attendance->clock_in)->format('H:i') }}</td>
-                        <td class="attendance__list__data">{{ \Carbon\Carbon::parse($attendance->clock_out)->format('H:i') }}</td>
-                        <td class="attendance__list__data">{{ \Carbon\Carbon::parse($attendance->total_break)->format('H:i') }}</td>
-                        <td class="attendance__list__data">{{ \Carbon\Carbon::parse($attendance->working_hours)->format('H:i') }}</td>
-                        <td class="attendance__list__data">
-                            <a href="{{ route('admin.detail', ['id' => $attendance->id]) }}">
-                                詳細
-                            </a>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+        <a class="attendance__list__prev-next-day" href="{{ url('/admin/attendance/list?day=' . $nextDay) }}"><span>翌日</span><img src="{{ asset('images/image_next.png') }}" alt="arrow_next"></a>
     </div>
+
+    <table class="attendance__list__table">
+        <thead>
+            <tr class="attendance__list__row">
+                <th class="attendance__list__label">名前</th>
+                <th class="attendance__list__label">出勤</th>
+                <th class="attendance__list__label">退勤</th>
+                <th class="attendance__list__label">休憩</th>
+                <th class="attendance__list__label">合計</th>
+                <th class="attendance__list__label">詳細</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($attendances as $attendance)
+                <tr class="attendance__list__row">
+                    <td class="attendance__list__data">{{ $attendance->user->name ?? '不明' }}</td>
+                    <td class="attendance__list__data">{{ $attendance->clock_in_formatted }}</td>
+                    <td class="attendance__list__data">{{ $attendance->clock_out_formatted }}</td>
+                    <td class="attendance__list__data">{{ $attendance->total_break_formatted }}</td>
+                    <td class="attendance__list__data">{{ $attendance->working_hours_formatted }}</td>
+                    <td class="attendance__list__data">
+                        <a href="{{ route('admin.detail', ['id' => $attendance->id]) }}">
+                            詳細
+                        </a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
+
 @endsection
--- src/resources/views/admin/attendance_staff_list.blade.php
+++ src/resources/views/admin/attendance_staff_list.blade.php
@@ -1,28 +1,30 @@
 @extends('layouts.app')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/auth/attendance_list.css')}}">
+<link rel="stylesheet" href="{{ asset('css/admin/attendance_staff_list.css')}}">
 @endsection
 
 @section('content')
-<div class="">
-    <h1>{{ str_replace([' ', '　'], '', $user->name) }}さんの一覧</h1>
+<div class="attendance__list__content">
+    <h1 class="attendance__list__header">{{ str_replace([' ', '　'], '', $user->name) }}さんの一覧</h1>
 
-    <h2>{{ $current->format('Y年n月') }} の日付一覧</h2>
+    <div class="attendance__list__month-select-box">
+        <a class="attendance__list__prev-next-month" href="{{ route('admin.attendance_staff_list', ['id' => $user->id, 'month' => $prevMonth]) }}"><img src="{{ asset('images/image_prev.png') }}" alt="arrow_prev"><span>前月</span></a>
+        <div class="attendance__list__this-month-calendar">
+            <img src="{{ asset('images/image_calendar.png') }}" alt="calendar">
+            <p class="attendance__list__this-month">{{ $current->format('Y/n') }}</p>
+        </div>
+        <a class="attendance__list__prev-next-month" href="{{ route('admin.attendance_staff_list', ['id' => $user->id, 'month' => $nextMonth]) }}"><span>翌月</span><img src="{{ asset('images/image_next.png') }}" alt="arrow_next"></a>
+    </div>
 
-    <p>
-        <a href="{{ route('admin.attendance_staff_list', ['id' => $user->id, 'month' => $prevMonth]) }}">← 前月</a> |
-        <a href="{{ route('admin.attendance_staff_list', ['id' => $user->id, 'month' => $nextMonth]) }}">翌月 →</a>
-    </p>
-
-    <table>
-        <tr>
-            <th>日付</th>
-            <th>出勤</th>
-            <th>退勤</th>
-            <th>休憩</th>
-            <th>合計</th>
-            <th>詳細</th>
+    <table class="attendance__list__table">
+        <tr class="attendance__list__row">
+            <th class="attendance__list__label">日付</th>
+            <th class="attendance__list__label">出勤</th>
+            <th class="attendance__list__label">退勤</th>
+            <th class="attendance__list__label">休憩</th>
+            <th class="attendance__list__label">合計</th>
+            <th class="attendance__list__label">詳細</th>
         </tr>
 
         @foreach ($dates as $date)
@@ -31,29 +33,26 @@
                 $formatted = $date->format('m/d') . '(' . $weekdays[$date->dayOfWeek] . ')';
 
                 // その日の勤怠データを探す
-                $attendanceForDate = $attendances->firstWhere('work_date', $date->toDateString());
+                $attendanceForDate = $attendancesByDate[$date->toDateString()] ?? null;
             @endphp
-            <tr>
-                <td>
+
+            <tr class="attendance__list__row">
+                <td class="attendance__list__data">
                     {{ $formatted }}
                 </td>
-                <td>
-                    {{ $attendanceForDate && $attendanceForDate->clock_in
-                        ? \Carbon\Carbon::parse($attendanceForDate->clock_in)->format('H:i')
-                        : '-' }}
+                <td class="attendance__list__data">
+                    {{ $attendanceForDate && $attendanceForDate->clock_in ? \Carbon\Carbon::parse($attendanceForDate->clock_in)->format('H:i') : '' }}
                 </td>
-                <td>
-                    {{ $attendanceForDate && $attendanceForDate->clock_out
-                        ? \Carbon\Carbon::parse($attendanceForDate->clock_out)->format('H:i')
-                        : '-' }}
+                <td class="attendance__list__data">
+                    {{ $attendanceForDate && $attendanceForDate->clock_out ? \Carbon\Carbon::parse($attendanceForDate->clock_out)->format('H:i') : '' }}
+                </td class="attendance__list__data">
+                <td class="attendance__list__data">
+                    {{ $attendanceForDate && $attendanceForDate->total_break ? \Carbon\Carbon::parse($attendanceForDate->total_break)->format('H:i') : '' }}
                 </td>
-                <td>
-                    {{ $attendanceForDate && $attendanceForDate->total_break ? \Carbon\Carbon::parse($attendanceForDate->total_break)->format('H:i') : '-' }}
+                <td class="attendance__list__data">
+                    {{ $attendanceForDate && $attendanceForDate->working_hours ? \Carbon\Carbon::parse($attendanceForDate->working_hours)->format('H:i') : '' }}
                 </td>
-                <td>
-                    {{ $attendanceForDate ? \Carbon\Carbon::parse($attendanceForDate->working_hours)->format('H:i') : '-' }}
-                </td>
-                <td>
+                <td class="attendance__list__data">
                     @if ($attendanceForDate)
                         <a href="{{ route('admin.detail', ['id' => $attendanceForDate->id]) }}">
                             詳細
@@ -66,6 +65,10 @@
         @endforeach
     </table>
 
-    <button>CSV出力</button>
+    <div class="attendance__staff__list__output-box">
+        <button class="attendance__staff__list__output btn" type="submit">
+            CSV出力
+        </button>
+    </div>
 </div>
 @endsection
